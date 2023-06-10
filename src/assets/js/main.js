7b57dd7d76ec0b168d790275ccd99e5bfa79bf76
Bakhriev <legendofpain5112@gmail.com>
2023-06-10 23:02:49 +0500

    end header

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,27 @@
-;(function burgerMenu() {
+function burgerMenu() {
   const burger = document.querySelector('.burger')
   const navigation = document.querySelector('.header__navigation')
   const overlay = document.querySelector('.overlay')
+  const navItems = document.querySelectorAll('.nav__item')
 
   burger.addEventListener('click', () => {
     burger.classList.toggle('active')
     navigation.classList.toggle('active')
     overlay.classList.toggle('active')
+    navItems.forEach(item => {
+      setTimeout(() => {
+        item.classList.toggle('active')
+      }, 600)
+    })
   })
 
   overlay.addEventListener('click', () => {
     burger.classList.remove('active')
     navigation.classList.remove('active')
     overlay.classList.remove('active')
+    navItems.forEach(item => {
+      item.classList.remove('active')
+    })
   })
 
   window.addEventListener('resize', () => {
@@ -20,9 +29,13 @@
       burger.classList.remove('active')
       navigation.classList.remove('active')
       overlay.classList.remove('active')
+      navItems.forEach(item => {
+        item.classList.remove('active')
+      })
     }
   })
-})()
+}
+burgerMenu()
 
 const dropdownInit = () => {
   document.addEventListener('click', e => {
@@ -37,6 +50,17 @@ const dropdownInit = () => {
       dropdown.classList.remove('active')
     })
   })
+  const allDropdowns = document.querySelectorAll('[data-dropdown]')
+  if (window.innerWidth > 992) {
+    allDropdowns.forEach(d => {
+      d.addEventListener('mouseover', () => {
+        d.classList.add('active')
+      })
+      d.addEventListener('mouseleave', () => {
+        d.classList.remove('active')
+      })
+    })
+  }
 }
 
 dropdownInit()
@@ -81,57 +105,58 @@ function loop() {
 animDots()
 
 const navLinks = document.querySelectorAll('.duplicate')
+if (window.innerWidth > 1024) {
+  navLinks.forEach(link => {
+    const spans1 = link.querySelectorAll('.v-text-1 span')
+    const spans2 = link.querySelectorAll('.v-text-2 span')
 
-navLinks.forEach(link => {
-  const spans1 = link.querySelectorAll('.v-text-1 span')
-  const spans2 = link.querySelectorAll('.v-text-2 span')
-
-  link.addEventListener('mouseover', () => {
-    {
-      let delay = 0
-      let duration = 250
+    link.addEventListener('mouseover', () => {
+      {
+        let delay = 0
+        let duration = 250
+        spans1.forEach(span => {
+          span.style.transition = `transform ${duration}ms ease-in-out`
+          span.style.transitionDelay = `${delay}ms`
+          span.style.transform = 'translate(0, -110%)'
+          if (delay < 100) {
+            delay += 20
+          } else {
+            delay += 5
+          }
+          if (duration < 400) {
+            duration += 80
+          } else {
+            duration += 10
+          }
+        })
+      }
+      {
+        let delay = 0
+        let duration = 250
+        spans2.forEach(span => {
+          span.style.transition = `transform ${duration}ms ease-in-out`
+          span.style.transitionDelay = `${delay}ms`
+          span.style.transform = 'translate(0, -100%)'
+          if (delay < 150) {
+            delay += 10
+          } else {
+            delay += 5
+          }
+          if (duration < 400) {
+            duration += 80
+          } else {
+            duration += 20
+          }
+        })
+      }
+    })
+    link.addEventListener('mouseleave', () => {
       spans1.forEach(span => {
-        span.style.transition = `transform ${duration}ms ease-in-out`
-        span.style.transitionDelay = `${delay}ms`
-        span.style.transform = 'translate(0, -110%)'
-        if (delay < 100) {
-          delay += 20
-        } else {
-          delay += 5
-        }
-        if (duration < 400) {
-          duration += 80
-        } else {
-          duration += 10
-        }
+        span.style.transform = `translate(0, 0)`
       })
-    }
-    {
-      let delay = 0
-      let duration = 250
       spans2.forEach(span => {
-        span.style.transition = `transform ${duration}ms ease-in-out`
-        span.style.transitionDelay = `${delay}ms`
-        span.style.transform = 'translate(0, -100%)'
-        if (delay < 150) {
-          delay += 10
-        } else {
-          delay += 5
-        }
-        if (duration < 400) {
-          duration += 80
-        } else {
-          duration += 20
-        }
+        span.style.transform = `translate(0, 100%)`
       })
-    }
-  })
-  link.addEventListener('mouseleave', () => {
-    spans1.forEach(span => {
-      span.style.transform = `translate(0, 0)`
-    })
-    spans2.forEach(span => {
-      span.style.transform = `translate(0, 100%)`
     })
   })
-})
+}
